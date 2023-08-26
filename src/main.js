cf5e9020db29c7c890976b9ac93fec15f2cc7cfa
Nguyen Tien Sy <nguyentiensy.nts.9796@gmail.com>
2023-08-26 09:17:52 +0700

    feat: Add Navigo -setup navigo router in main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
-document.querySelector("#app").innerHTML = `
-    <h1>Nguyen tien sy</h1>
-`;
+// eslint-disable-next-line import/no-extraneous-dependencies
+import Navigo from "navigo";
+
+const router = new Navigo("/", { linksSelector: "a" });
+router.on({
+    "/": () => console.log("Homepage"),
+    "/about": () => console.log("AboutPage"),
+    "/product": () => console.log("ProductPage"),
+});
+router.resolve();
